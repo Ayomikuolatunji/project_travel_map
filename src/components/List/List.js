469f9123bebc31f2ddb5c7367b849d8998654667
Ayomikuolatunji <ayomikuolatunji@gmail.com>
2022-03-23 17:40:01 +0100

    type and rating

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,13 +3,13 @@ import useStyles from './listStyle';
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
 import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
-export default function List({places,childClicked,loading,rating,setRating,setType},type){
+export default function List({places,childClicked,loading,rating,setRating,setType,type}){
     const classes=useStyles();
     const [elRefs, setElRefs] = React.useState([]);
       
      
     
-  
+    console.log(type)
     React.useEffect(() => {
       setElRefs((refs) => Array(places?.length).fill().map((_, i) => refs[i] || createRef()));
     }, [places]);
@@ -34,7 +34,7 @@ export default function List({places,childClicked,loading,rating,setRating,setTy
            </FormControl>
            <FormControl className={classes.formControl}>
             <InputLabel id="rating">Rating</InputLabel>
-            <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
+            <Select id="rating"  onChange={(e) => setRating(e.target.value)}>
               <MenuItem value="">All</MenuItem>
               <MenuItem value="3">Above 3.0</MenuItem>
               <MenuItem value="4">Above 4.0</MenuItem>
