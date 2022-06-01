023d4aa3d0fe531c4985fecc2d2bdeee355a2705
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-01 21:48:04 +0100

    map finally figured out

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Address from './components/address/Address'
-import AttractionHotelsRes from './pages/AttractionHotelsRes'
+// import AttractionHotelsRes from './pages/AttractionHotelsRes'
 
 const App = () => {
   return (
--- src/components/address/Address.js
+++ src/components/address/Address.js
@@ -1,42 +1,82 @@
-import React from 'react'
+import React, { useCallback, useMemo } from 'react'
 import axios from "axios"
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+
 
 const GOOGLE_API_KEY='AIzaSyAvTG8YHyz82spRn_az1S6Nrc_7atr5n1E'
 
 const Address = () => {
+  const [lat,setLat] = React.useState(0)
+  const [lng,setLng]=React.useState(0)
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyAvTG8YHyz82spRn_az1S6Nrc_7atr5n1E"
+  })
+
+  const [map, setMap] = React.useState(null)
   const [address, setAddress] = React.useState('')
 
 
-    const searchAddress=async(event)=>{
-      event.preventDefault()
-     try {
-      const response= await  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)}&key=${GOOGLE_API_KEY}`)
-      console.log(response)
-      const coordinates = response.data.results[0].geometry.location;
-      // eslint-disable-next-line no-undef
-       new google.maps.Map(document.getElementById("map"), {
-        center:coordinates,
-        zoom: 8,
-      });
-     } catch (err) {
-          console.log(err)
-     }
+  const containerStyle = {
+    width: '800px',
+    height: '400px'
+  };
+  
+  const center = useMemo(() => {
+      return {
+        lat: lat,
+        lng: lng
+      }
+  },[lat,lng])
+  console.log(lat,lng);
+
+
+  const searchFunction=async(event)=>{
+    event.preventDefault()
+    try {
+        const response= await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)}&key=${GOOGLE_API_KEY}`)
+        console.log(response)
+         setLat(response.data.results[0].geometry.location.lat)
+         setLng(response.data.results[0].geometry.location.lng)
+      } catch (err) {
+            console.log(err)
+      }
     }
+    const onLoad = useCallback(function callback(map) {
+         const bounds = new window.google.maps.LatLngBounds(parseFloat(center));
+         map.fitBounds(bounds);
+    }, [center]);
 
 
-
+    const onUnmount = React.useCallback(function callback(map) {
+      setMap(null)
+    }, [])
+   
 
   return (
     <div>
-      <form id="form" onSubmit={searchAddress}>
-        <input type="text" id="address" name="address" className="p-2" placeholder="type your valid address here.." 
-        onChange={(e)=>setAddress(e.target.value)}
+      <form id="form" onSubmit={searchFunction}>
+        <input type="text" id="address" name="address" className="p-2" placeholder="type your valid address here.."
+         onChange={(e)=>setAddress(e.target.value)}
         />
         <button type="submit" className="btn btn-primary">SEARCH ADDRESS</button>
       </form>
-      <div id="map">
-        <p>Please enter an address!</p>
-      </div>
+      {
+        isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            { /* Child components, such as markers, info windows, etc. */ }
+            <></>
+          </GoogleMap>
+      ) : <></>
+      }
     </div>
   )
 }
