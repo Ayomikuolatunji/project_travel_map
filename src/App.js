3c6acd255a8601d31c84f6f3ad42f9f110304f67
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-01 20:03:55 +0100

    I am starting a new page where users can search for any address on the map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Address from './components/address/Address'
 import AttractionHotelsRes from './pages/AttractionHotelsRes'
 
 const App = () => {
   return (
     <div>
       <AttractionHotelsRes/>
+      <Address/>
     </div>
   )
 }
