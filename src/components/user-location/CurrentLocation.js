9a0d9516888fd4976cd1f9beea02d81e64d9edb4
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-02 13:38:42 +0100

    get user current addres from google api

--- src/components/user-location/CurrentLocation.js
+++ src/components/user-location/CurrentLocation.js
@@ -16,15 +16,24 @@ const CurrentLocation = () => {
         if(coords.lat || coords.lng){
         const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=AIzaSyAvTG8YHyz82spRn_az1S6Nrc_7atr5n1E`)
          const data = await response.json()
+         console.log(data)
         setLoactions({location:data.plus_code.compound_code})
         }
       }
       getCurrentLocation()
 
   },[coords])
-
+  function removeFirstword(name) {
+    const names = name.split(" ").map(function (word) {
+         return word;
+     });
+     const [, ...rest] = names;
+    return rest.join(" ");
+ }
   return (
-    <div> <h1 className='text-xl'>{location?.location.split("RXWF+VRQ")}</h1> </div>
+    <div> 
+      <h1 className='sm:text-2xl text-xl'>{removeFirstword(location?.location)}</h1>
+    </div>
   )
 }
 
